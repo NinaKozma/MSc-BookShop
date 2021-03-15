aaa23b11249b081d925c7831fe71240c9402b147
NinaKozma <nina.kozma@gmail.com>
2021-03-15 18:31:46 +0100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Functional component - vraca ceo output; JSX
+/*Return obavezno vraca samo jedan element i to moze
+biti bilo koji element*/
+function App() {
+  return (
+    <>
+     <h1>Welcome to All Booked Up!</h1>
+    </> 
+  )
+}
+
+export default App;
